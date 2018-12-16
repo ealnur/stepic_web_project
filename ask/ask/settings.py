43f78119215e337eafc748f454af4169de74da31
ealnur <ilnur.ismagilov@gmail.com>
2018-12-16 08:32:26 +0000

    save2

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&=bvfv2gmz=w!vfou1dm*d)t@ozb-wq4t9soywc2zk1gx9djp3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'qa',
 ]
 
 MIDDLEWARE = [
--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -13,17 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 from qa import views
 
+print 22222222
+
 urlpatterns = [
+    url(r'^', include('qa.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^$',),
-    url(r'^login/$'),
-    url(r'^signup/$'),
-    url(r'^question/(\d+)/$', qa.views.test),
-    url(r'^ask/$'),
-    url(r'^popular/$'),
-    url(r'^new/$')
+#    url(r'^login/$'),
+#    url(r'^signup/$'),
+#    url(r'^question/(\d+)/$', qa.views.test),
+#    url(r'^ask/$'),
+#    url(r'^popular/$'),
+#    url(r'^new/$')
 ]
+
+print urlpatterns
+
