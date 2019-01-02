4faf733d8e0dede4a38f1371df7d4712cca0cf36
ealnur <ilnur.ismagilov@gmail.com>
2019-01-02 06:02:13 +0000

    init mysql

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
+#sudo /etc/init.d/mysql start
+#mysql -uroot -e "create database webbase"
+#mysql -uroot -e "create user web"
 sudo ln -sf ~/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf ~/web/etc/gunicorn.conf /etc/gunicorn.d/test
