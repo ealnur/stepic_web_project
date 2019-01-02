59fe623a55e8127d1b72bff5d7794a129d2709ba
ealnur <ilnur.ismagilov@gmail.com>
2019-01-02 06:35:03 +0000

    uncomment init mysql

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
-#sudo /etc/init.d/mysql start
-#mysql -uroot -e "create database webbase"
-#mysql -uroot -e "create user web"
+sudo /etc/init.d/mysql start
+mysql -uroot -e "create database webbase"
+mysql -uroot -e "create user web"
 sudo ln -sf ~/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf ~/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
-#sudo /etc/init.d/mysql start
+
