5051c1c260b06735bc9ffb162c0d1bd8cebbf681
ealnur <ilnur.ismagilov@gmail.com>
2019-01-09 05:44:07 +0000

    fix migrate

--- init.sh
+++ init.sh
@@ -1,10 +1,8 @@
 sudo /etc/init.d/mysql start
-#mysql -uroot -e "create user 'admin'@'localhost'"
-#mysql -uroot -e "set password for 'admin'@'localhost' = password('pass111')"
-#mysql -uroot -e "create database mybase"
 mysql -uroot -e "create database stepic_web;"
-#mysql -uroot -e "grant all on mybase.* to 'admin'@'localhost'"
 mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
+~/web/ask/manage.py makemigrations
+~/web/ask/manage.py migrate
 sudo ln -sf ~/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf ~/web/etc/gunicorn.conf /etc/gunicorn.d/test
