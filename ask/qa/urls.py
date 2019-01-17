c2dfc8b059c8064f5fb43c50c0ac0966ce4e21a1
ealnur <ilnur.ismagilov@gmail.com>
2019-01-17 05:10:23 +0000

    try to fix view

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from qa.views import test, post_list_all
 
 urlpatterns = [
-  url(r'^\w+$', post_list_all, name='post-list-all'),
+  url(r'^', post_list_all, name='post-list-all'),
 ]
 
 
