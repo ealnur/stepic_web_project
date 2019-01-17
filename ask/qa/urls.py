f856ea5903468e335ca81866dcfa7905a940ad54
ealnur <ilnur.ismagilov@gmail.com>
2019-01-17 05:01:38 +0000

    add post_list_all to view and urls

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 
-from qa.views import test
+from qa.views import test, post_list_all
 
 urlpatterns = [
-  url(r'^', test),
+  url(r'^\w+$', post_list_all, name='post-list-all'),
 ]
 
 
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 # Create your views here.
 from django.http import HttpResponse
 def test(request, *args, **kwargs):
 	return HttpResponse('OK\n')
 
+
+def post_list_all(request):
+  questions = Question.objects.new()
+  limit = request.GET.get('limit', 10)
+  page = request.GET.get('page', 1)
+  paginator = Paginator(questions, limit)
+  paginator.baseurl = '/?page='
+  page = paginator.page(page)
+  return render(request, 'qa/question.html', {
+    'questions': page.object_list,
+    'paginator': paginator, 'page': page,  
+  })
